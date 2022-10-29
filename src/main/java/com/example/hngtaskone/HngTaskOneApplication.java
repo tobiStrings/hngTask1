c8504fbf7f20cfb4b74fe244e24e40fe8a097893
tobiStrings <ligalititobiloluwa2005@gmail.com>
2022-10-29 15:57:13 +0100

    first commit

---
+++ src/main/java/com/example/hngtaskone/HngTaskOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.hngtaskone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngTaskOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HngTaskOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hngtaskone/HngTaskOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hngtaskone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngTaskOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
